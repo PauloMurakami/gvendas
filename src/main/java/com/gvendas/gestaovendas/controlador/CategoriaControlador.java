1dd29b435d99c77de5940e003d03d9f9476fa1dd
Paulo Murakami <ppmura13@gmail.com>
2023-03-03 14:19:51 -0300

    Feat- subindo o atualizar Categoria

--- src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
+++ src/main/java/com/gvendas/gestaovendas/controlador/CategoriaControlador.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,8 +29,8 @@ public class CategoriaControlador {
     }
 
     @GetMapping("/{codigo}")
-    public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable(name = "codigo") Long codigo){
-        Optional<Categoria> categoria = categoriaServico.buscarPorId(codigo);
+    public ResponseEntity<Optional<Categoria>> buscarPorCodigo(@PathVariable(name = "codigo") Long codigo){
+        Optional<Categoria> categoria = categoriaServico.buscarPorCodigo(codigo);
         return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
     }
 
@@ -37,5 +38,10 @@ public class CategoriaControlador {
     public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria){
         Categoria categoriaSalva = categoriaServico.salvar(categoria);
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+    }
+    
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Categoria> atualizar(@PathVariable(name = "codigo") Long codigo, @RequestBody Categoria categoria){
+        return ResponseEntity.ok(categoriaServico.atualizar(codigo, categoria));
     }
 }
--- src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
+++ src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
@@ -3,10 +3,15 @@ package com.gvendas.gestaovendas.servico;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gvendas.gestaovendas.repositorio.CategoriaRepositorio;
+
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+
 import com.gvendas.gestaovendas.entidades.Categoria;
 
 
@@ -19,12 +24,26 @@ public class CategoriaServico {
     public List<Categoria> listarTodas(){
         return categoriaRepositorio.findAll();
     }
-    
-    public Optional<Categoria> buscarPorId(Long codigo){
+
+    public Optional<Categoria> buscarPorCodigo(Long codigo){
         return categoriaRepositorio.findById(codigo);
     }
 
     public Categoria salvar(Categoria categoria){
         return categoriaRepositorio.save(categoria);
+    }
+
+    public Categoria atualizar(Long codigo, Categoria categoria){
+       Categoria categoriaSalvar =  validarCategoriaExiste(codigo);
+       BeanUtils.copyProperties(categoria, categoriaSalvar, "codigo");
+       return categoriaRepositorio.save(categoriaSalvar);
+    }
+
+    private Categoria validarCategoriaExiste(Long codigo) {
+        Optional<Categoria> categoria = buscarPorCodigo(codigo);
+        if(categoria.isEmpty()){
+            throw new EmptyResultDataAccessException(1);
+        }
+        return categoria.get();
     }
 }

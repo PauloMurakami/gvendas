bb98eb89c5a1eca486505606cd4b32b19dbe2a03
Paulo Murakami <ppmura13@gmail.com>
2023-03-07 14:41:43 -0300

    Adicionando tratamento de erros

--- src/main/java/com/gvendas/gestaovendas/entidades/Categoria.java
+++ src/main/java/com/gvendas/gestaovendas/entidades/Categoria.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "categoria")
@@ -17,6 +20,8 @@ public class Categoria {
     private long codigo;
 
     @Column(name = "nome")
+    @NotBlank(message = "Nome")
+    @Length(min = 3, max = 50, message = "Nome")
     private String nome;
 
     public long getCodigo() {
